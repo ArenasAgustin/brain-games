15b86eb594014abc933c9625f9c5aa996acff19b
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-01-02 10:56:24 -0300

    level 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import SelectLevel from "./pages/SelectLevel/SelectLevel";
 import Level1 from "./pages/Level1/Level1Page";
 import Level2 from "./pages/Level2/Level2Page";
-import SelectLevel from "./pages/SelectLevel/SelectLevel";
-import Home from "./pages/Home/Home";
+import Level3 from "./pages/Level3/Level3Page";
 import "./App.scss";
-
 
 function App() {
   return (
@@ -18,6 +18,8 @@ function App() {
         <Route path="/select/level1" element={<Level1 />} />
 
         <Route path="/select/level2" element={<Level2 />} />
+
+        <Route path="/select/level3" element={<Level3 />} />
       </Routes>
     </div>
   );
