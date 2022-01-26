d74fff070b1391c4cc2f3a43768fb5cc752b8c20
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-01-26 11:25:35 -0300

    new home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './Home.scss';
+import "./Home.scss";
 
 export default function Home() {
   return (
     <div className="home__container">
+      <div className="home__title-div">
+        <h1 className="home__title">
+          <span className="home__title-brain">Brain</span> Games{" "}
+          <span className="home__title-es">ES</span>
+        </h1>
+      </div>
+
       <div className="home__button-container">
         <NavLink className="home__button" to={`select`}>
           <p className="home__button-text">Elegir nivel</p>
