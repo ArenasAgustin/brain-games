204839fd27c643ec6337eba5fe9553643c23ba59
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-02-11 15:51:42 -0300

    level11

---
+++ src/components/Level11/Level11.tsx
@@ -0,0 +1,34 @@
+import "./Level11.scss";
+
+export default function Level11() {
+  return (
+    <div className="level11__container">
+      <div className="level11__puzzle">
+        <span className="level11__file">
+          <span className="green">H</span>
+          <span className="red">R</span>
+          <span className="grey">M</span>
+          <span className="blue">E</span>
+        </span>
+
+        <span className="level11__file">
+          <span className="red">B</span>
+          <span className="grey">L</span>
+          <span className="green">A</span>
+        </span>
+
+        <span className="level11__file">
+          <span className="green">C</span>
+          <span className="grey">R</span>
+          <span className="red">A</span>
+        </span>
+
+        <span className="level11__file">
+          <span className="grey">S</span>
+          <span className="green">K</span>
+          <span className="red">T</span>
+        </span>
+      </div>
+    </div>
+  );
+}
