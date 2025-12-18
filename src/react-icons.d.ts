8ed9038be5a4b259a9935de949f08724f7dd7aa1
Agustin Arenas <arenasagustin7@gmail.com>
2025-12-17 23:39:15 -0300

    chore: update TypeScript and @types dependencies; add type declarations for react-icons

---
+++ src/react-icons.d.ts
@@ -0,0 +1,16 @@
+declare module "react-icons/fa" {
+  import { ComponentType, SVGAttributes } from "react";
+  export const FaCheck: ComponentType<SVGAttributes<SVGElement>>;
+}
+
+declare module "react-icons/io5" {
+  import { ComponentType, SVGAttributes } from "react";
+  export const IoCloseSharp: ComponentType<SVGAttributes<SVGElement>>;
+  export const IoChevronForwardSharp: ComponentType<SVGAttributes<SVGElement>>;
+}
+
+declare module "react-icons/gi" {
+  import { ComponentType, SVGAttributes } from "react";
+  export const GiPadlock: ComponentType<SVGAttributes<SVGElement>>;
+  export const GiPadlockOpen: ComponentType<SVGAttributes<SVGElement>>;
+}
